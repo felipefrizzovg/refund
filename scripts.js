a4eb0c6dddcd1c5cf6ee4647a86be8ee7cee500a
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-05-16 14:24:16 -0300

    Added remove icon and expense amount

--- scripts.js
+++ scripts.js
@@ -60,8 +60,34 @@ function expenseAdd(newExpense) {
     expenseIcon.setAttribute('src', `img/${newExpense.category_id}.svg`)
     expenseIcon.setAttribute('alt', newExpense.category_name)
 
+    // cria as informações da despesa
+    const expenseInfo = document.createElement("div")
+    expenseInfo.classList.add("expense-info")
+
+    // cria o nome da despesa
+    const expenseName = document.createElement("strong")
+    expenseName.textContent = newExpense.expense
+
+    // cria a categoria da despesa
+    const expenseCategory = document.createElement("span")
+    expenseCategory.textContent = newExpense.category_name
+
+    // Adiciona o nome e categoria dentro da div de info
+    expenseInfo.append(expenseName, expenseCategory)
+
+    // Cria o valor da despesa
+    const expenseAmount = document.createElement("span")
+    expenseAmount.classList.add('expense-amount')
+    expenseAmount.innerHTML = `<small>R$</small>${newExpense.amount.toUpperCase().replace('R$', '')}`
+
+    // Cria o icone de remover
+    const removeIcon = document.createElement("img")
+    removeIcon.classList.add("remove-icon")
+    removeIcon.setAttribute('src', 'img/remove.svg')
+    removeIcon.setAttribute('alt', 'remover')
+
     // adiciona as informações no item
-    expenseItem.append(expenseIcon)
+    expenseItem.append(expenseIcon, expenseInfo, expenseAmount, removeIcon)
 
     // adiciona o item na lista
     expenseList.append(expenseItem)
